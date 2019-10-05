7ad19fee5b8f888b3ce92580b3b66ee20566d714
Andras Szalai <andras_szalai@epam.com>
2019-10-05 12:05:56 +0200

    preflop dumb name

--- preflop.py
+++ preflop.py
@@ -1,6 +1,9 @@
 import sys
 
 class Preflop:
+    def __init__(self, game_state):
+        player = game_state['players'][game_state['in_action']]
+        hole_cards = player['hole_cards']
 
-    def TODO(self):
-        0
+    def name(self):
+        return "Pair of A"
--- test/test.py
+++ test/test.py
@@ -3,6 +3,24 @@ from preflop import Preflop
 
 class I_can_has_unittest(unittest.TestCase):
 
+    # diamonds clubs hearts spades
+    def mock_hole(self, cards):
+        game={
+            'in_action':'dummy',
+            'players':{'dummy':{
+                'hole_cards':[
+                    {'rank':'A',
+                     'suit':'hearts'}
+                ]
+            }}
+        }
+
+        return game
+
     def test_preflop_class(self):
-        x=Preflop()
+        x=Preflop(self.mock_hole(["AD", "2H"]))
         self.assertIsInstance(x, Preflop)
+
+    def test_name(self):
+        x=Preflop(self.mock_hole(["AD", "AD"]))
+        self.assertEqual(x.name(), "Pair of A")
