257db0ae90f7a4ac3b9a44b115f93e530d53eaed
Péter Mikó <miko.peti1993@gmail.com>
2019-10-05 12:25:47 +0200

    Same kind cards

--- Hand.py
+++ Hand.py
@@ -15,12 +15,28 @@ class HandRank(Enum):
 	STRAIGHT_FLUSH = 9
 
 def GetHandRank(CardList):
+	hand = HandRank.HIGHCARD
 	CardList.sort(key=orderCardsByRank)
 	Ranks = []
-	for i in range(2, 5):
+	for i in range(2, 14):
 		Ranks.append(0)
 	for card in CardList:
 		Ranks[card.Rank-2] += 1
+	max = 0
+	Ranks.sort(reverse = True)
+	if Ranks[0] == 4:
+		return HandRank.POKER
+	if Ranks[0] == 3:
+		if Ranks[1] >= 2:
+			return HandRank.FULLHOUSE
+		else:
+			return HandRank.THREE_OF_A_KIND
+	if Ranks[0] == 2:
+		if Ranks[1] == 2:
+			return HandRank.TWO_PAIR
+		else:
+			return HandRank.PAIR
+
 
 class Hand:
 	cards = []
