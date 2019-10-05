af50d41e40432a27f3da289983935ef89aca2a13
Andras Szalai <andras_szalai@epam.com>
2019-10-05 15:04:00 +0200

    preflop card scores

--- preflop.py
+++ preflop.py
@@ -10,6 +10,15 @@ class Preflop:
         player = game_state['players'][game_state['in_action']]
         self.hole_cards = player['hole_cards']
 
+    def high_card(self):
+        c1=self.hole_cards[0]
+        cr1 = card_rank(c1)
+        c2 = self.hole_cards[1]
+        cr2 =card_rank(c2)
+        if(cr1>=cr2):
+            return c1
+        return c2
+
     def name(self):
         c1=self.hole_cards[0];
         cr1= card_rank(c1);
@@ -19,10 +28,13 @@ class Preflop:
             return 'Pair of {rank}'.format(rank=self.hole_cards[0]['rank'])
 
         if cr2>cr1:
-            high_card=c2
             low_card=c1
         else:
-            high_card = c1
             low_card = c2
 
-        return 'High card {r1}, low card {r2}'.format(r1=high_card['rank'], r2=low_card['rank'])
+        return 'High card {r1}, low card {r2}'.format(r1=self.high_card()['rank'], r2=low_card['rank'])
+
+    high_card_values=[-666, -42, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 7, 8, 10]
+    def high_card_value(self):
+        r = card_rank(self.high_card())
+        return self.high_card_values[r]
--- test/test_preflop.py
+++ test/test_preflop.py
@@ -1,8 +1,9 @@
 import unittest
-from preflop import Preflop
+from preflop import Preflop, card_rank
+
 
 def mock_card(card):
-    return {'rank': card[0:1], 'suit': card[1:2]}
+    return {'rank': card[0:-1], 'suit': card[-2:-1]}
 
 def mock_hole_cards(cards):
     return [mock_card(card) for card in cards]
@@ -38,3 +39,47 @@ class I_can_has_unittest(unittest.TestCase):
     def test_name_high_A_rev(self):
         x=Preflop(self.mock_hole(["KD", "AD"]))
         self.assertEqual("High card A, low card K", x.name())
+
+    ######################################################
+
+    def test_card_rank_A(self):
+        self.assertEqual(card_rank({'rank':'A'}), 14)
+
+    def test_high_value_A(self):
+        self.assertEqual(10, Preflop(self.mock_hole(["AD", "2D"])).high_card_value())
+
+    def test_high_value_K(self):
+        self.assertEqual(8, Preflop(self.mock_hole(["KD", "2D"])).high_card_value())
+
+    def test_high_value_Q(self):
+        self.assertEqual(7, Preflop(self.mock_hole(["QD", "2D"])).high_card_value())
+
+    def test_high_value_J(self):
+        self.assertEqual(6, Preflop(self.mock_hole(["JD", "2D"])).high_card_value())
+
+    def test_high_value_10(self):
+        self.assertEqual(5, Preflop(self.mock_hole(["10D", "2D"])).high_card_value())
+
+    def test_high_value_9(self):
+        self.assertEqual(4.5, Preflop(self.mock_hole(["9D", "2D"])).high_card_value())
+
+    def test_high_value_8(self):
+        self.assertEqual(4, Preflop(self.mock_hole(["8D", "2D"])).high_card_value())
+
+    def test_high_value_7(self):
+        self.assertEqual(3.5, Preflop(self.mock_hole(["7D", "2D"])).high_card_value())
+
+    def test_high_value_6(self):
+        self.assertEqual(3, Preflop(self.mock_hole(["6D", "2D"])).high_card_value())
+
+    def test_high_value_5(self):
+        self.assertEqual(2.5, Preflop(self.mock_hole(["5D", "2D"])).high_card_value())
+
+    def test_high_value_4(self):
+        self.assertEqual(2, Preflop(self.mock_hole(["4D", "2D"])).high_card_value())
+
+    def test_high_value_3(self):
+        self.assertEqual(1.5, Preflop(self.mock_hole(["3D", "2D"])).high_card_value())
+
+    def test_high_value_2(self):
+        self.assertEqual(1, Preflop(self.mock_hole(["2D", "2D"])).high_card_value())
