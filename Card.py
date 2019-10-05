e304536c508c0bf258c0e9c546252152216ccc68
Péter Mikó <miko.peti1993@gmail.com>
2019-10-05 15:31:01 +0200

    hand rank impl

--- Card.py
+++ Card.py
@@ -1,59 +1,61 @@
 from enum import Enum
 
 class Suits(Enum):
-	SPADE = 1
-	HEART = 2
-	DIAMOND = 3
-	CLUB = 4
+    SPADE = 1
+    HEART = 2
+    DIAMOND = 3
+    CLUB = 4
 
 class Rank(Enum):
-	TWO = 2
-	THREE = 3
-	FOUR = 4
-	FIVE = 5
-	SIX = 6
-	SEVEN = 7
-	EIGHT = 8
-	NINE = 9
-	TEN = 10
-	JACK = 11
-	QUEEN = 12
-	KING = 13
-	ACE = 14
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
 
 
 
 class Card:
-	suit = None
-	rank = None
-	def __init__ (self):
-		return
+    def __init__ (osuit, orank):
+        self.suit = osuit
+        self.rank = orank
+    def __init__ ( other_card ):
+        self.suit = other.suit
+        self.rank = other.rank
 
 def getCardFromJson (JsonObj):
-	x = Card()
-	jsuit = JsonObj["suit"]
-	if (jsuit == "spades"):
-		x.suit = Suits.SPADE
-	if (jsuit == "hearts"):
-		x.suit = Suits.HEART
-	if (jsuit == "diamonds"):
-		x.suit = Suits.DIAMOND
-	if (jsuit == "clubs"):
-		x.suit = Suits.CLUB
-	jrank = JsonObj["rank"]
-	if ( jrank == "A" ):
-		x.rank = Rank.ACE
-	elif ( jrank == "K" ):
-		x.rank = Rank.KING
-	elif ( jrank == "Q" ):
-		x.rank = Rank.QUEEN
-	elif ( jrank == "J" ):
-		x.rank = Rank.JACK
-	else:
-		x.rank = Rank(int(jrank))
-	return x
+    x = Card()
+    jsuit = JsonObj["suit"]
+    if (jsuit == "spades"):
+        x.suit = Suits.SPADE
+    if (jsuit == "hearts"):
+        x.suit = Suits.HEART
+    if (jsuit == "diamonds"):
+        x.suit = Suits.DIAMOND
+    if (jsuit == "clubs"):
+        x.suit = Suits.CLUB
+    jrank = JsonObj["rank"]
+    if ( jrank == "A" ):
+        x.rank = Rank.ACE
+    elif ( jrank == "K" ):
+        x.rank = Rank.KING
+    elif ( jrank == "Q" ):
+        x.rank = Rank.QUEEN
+    elif ( jrank == "J" ):
+        x.rank = Rank.JACK
+    else:
+        x.rank = Rank(int(jrank))
+    return x
 
 
 
 def orderCardsByRank (x, y):
-	return x.Rank > y.Rank
+    return x.Rank > y.Rank
--- Hand.py
+++ Hand.py
@@ -2,43 +2,76 @@ from enum import Enum
 
 from Card import orderCardsByRank
 
-
 class HandRank(Enum):
-	HIGHCARD = 1
-	PAIR = 2
-	TWO_PAIR = 3
-	THREE_OF_A_KIND = 4
-	STRAIGHT = 5
-	FLUSH = 6
-	FULLHOUSE = 7
-	POKER = 8
-	STRAIGHT_FLUSH = 9
+    HIGHCARD = 1
+    PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULLHOUSE = 7
+    POKER = 8
+    STRAIGHT_FLUSH = 9
 
 def GetHandRank(CardList):
-	hand = HandRank.HIGHCARD
-	CardList.sort(key=orderCardsByRank)
-	Ranks = []
-	for i in range(2, 14):
-		Ranks.append(0)
-	for card in CardList:
-		Ranks[card.Rank-2] += 1
-	max = 0
-	Ranks.sort(reverse = True)
-	if Ranks[0] == 4:
-		return HandRank.POKER
-	if Ranks[0] == 3:
-		if Ranks[1] >= 2:
-			return HandRank.FULLHOUSE
-		else:
-			return HandRank.THREE_OF_A_KIND
-	if Ranks[0] == 2:
-		if Ranks[1] == 2:
-			return HandRank.TWO_PAIR
-		else:
-			return HandRank.PAIR
+    hand = HandRank.HIGHCARD
+    CardList.sort(key=orderCardsByRank) 
+    Ranks = []
+    for i in range(2, 15):
+        Ranks.append(0)
+    for card in CardList:
+        Ranks[card.Rank-2] += 1
+    if len(Cardlist) >= 5: # check for straight
+        con = 0 # continous cards amount
+        lastRank = 0
+        for i in range(2,15):
+            if lastRank == 0 and Ranks[i-2] != 0:
+                lastRank = i
+                con = 1
+            elif lastRank+1 == i:
+                lastrank += 1
+                con += 1
+            else:
+                con = 0
+            if con == 5:
+                hand = HandRank.STRAIGHT
+    max = 0
+    Ranks.sort(reverse = True)
+    if len(CardList) >= 5: #check for flush
+        suitList = {0,0,0,0}
+        for card in CardList:
+            suitList[int(card.suit)] += 1
+        for i in range(0,4):
+            if i >= 5:
+                if hand == HandRank.STRAIGHT:
+                    return HandRank.STRAIGHT_FLUSH # todo check for royal?
+                else:
+                    hand = HandRank.FLUSH
+    if Ranks[0] == 4:
+        return HandRank.POKER
+    if Ranks[0] == 3:
+        if Ranks[1] >= 2:
+            return HandRank.FULLHOUSE
+        else:
+            return HandRank.THREE_OF_A_KIND
+    if Ranks[0] == 2:
+        if Ranks[1] == 2:
+            return HandRank.TWO_PAIR
+        else:
+            return HandRank.PAIR
 
+def PreFlop (CardsInHand):
+    return 1000 # todo
 
+def Flop (CardsInHand, TableCards):
+    return PreFlop(CardsInHand) # todo
+
+def Turn  (CardsInHand, TableCards):
+    return Flop(CardsInHand,TableCards) # todo
+
+def River (CardsInHand, TableCards):
+    return Turn(CardsInHand, TableCards) # todo
 class Hand:
-	cards = []
+    cards = []
 
-	
+    
