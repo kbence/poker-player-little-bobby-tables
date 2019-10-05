523238eeb1801c3f23d284ddbc6967bcffd39236
Andras Szalai <andras_szalai@epam.com>
2019-10-05 12:26:57 +0200

    preflop dumb name 2

--- preflop.py
+++ preflop.py
@@ -3,7 +3,12 @@ import sys
 class Preflop:
     def __init__(self, game_state):
         player = game_state['players'][game_state['in_action']]
-        hole_cards = player['hole_cards']
+        self.hole_cards = player['hole_cards']
 
     def name(self):
-        return "Pair of A"
+        r1=self.hole_cards[0]['rank'];
+        r2=self.hole_cards[1]['rank'];
+        if r1 == r2:
+            return 'Pair of {rank}'.format(rank=self.hole_cards[0]['rank'])
+
+        return 'High card {r1}, low card {r2}'.format(r1=r1, r2=r2)
--- test/test.py
+++ test/test.py
@@ -1,6 +1,12 @@
 import unittest
 from preflop import Preflop
 
+def mock_card(card):
+    return {'rank': card[0:1], 'suit': card[1:2]}
+
+def mock_hole_cards(cards):
+    return [mock_card(card) for card in cards]
+
 class I_can_has_unittest(unittest.TestCase):
 
     # diamonds clubs hearts spades
@@ -8,19 +14,23 @@ class I_can_has_unittest(unittest.TestCase):
         game={
             'in_action':'dummy',
             'players':{'dummy':{
-                'hole_cards':[
-                    {'rank':'A',
-                     'suit':'hearts'}
-                ]
+                'hole_cards': mock_hole_cards(cards)
             }}
         }
-
         return game
 
     def test_preflop_class(self):
         x=Preflop(self.mock_hole(["AD", "2H"]))
         self.assertIsInstance(x, Preflop)
 
-    def test_name(self):
+    def test_name_pair_A(self):
         x=Preflop(self.mock_hole(["AD", "AD"]))
         self.assertEqual(x.name(), "Pair of A")
+
+    def test_name_pair_K(self):
+        x=Preflop(self.mock_hole(["KD", "KD"]))
+        self.assertEqual(x.name(), "Pair of K")
+
+    def test_name_high_A(self):
+        x=Preflop(self.mock_hole(["AD", "KD"]))
+        self.assertEqual(x.name(), "High card A, low card K")
